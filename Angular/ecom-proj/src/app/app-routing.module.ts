aa85129168186e48e96f6677be541e7506cd5b35
Devendra <devendra_dewangan@outlook.com>
2024-01-13 17:16:21 +0530

    service http-request datatype

--- Angular/ecom-proj/src/app/app-routing.module.ts
+++ Angular/ecom-proj/src/app/app-routing.module.ts
@@ -3,19 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { Page404Component } from './page404/page404.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:HomeComponent
+    path: '',
+    component: HomeComponent
   },
   {
-    path:'seller-auth',
-    component:SellerAuthComponent
+    path: 'seller-auth',
+    component: SellerAuthComponent
+  }, 
+  {
+    path: 'seller-home',
+    component: SellerHomeComponent
   },
   {
-    path:'**',
-    component:Page404Component
+    path: '**',
+    component: Page404Component
   }
 ];
 
--- Angular/ecom-proj/src/app/seller-auth/seller-auth.component.ts
+++ Angular/ecom-proj/src/app/seller-auth/seller-auth.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import {Router} from '@angular/router'
+import { SignUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit {
+  constructor(private seller:SellerService,private router:Router){}
 
-  signUp(data:object):void{
-    console.warn(data);
+  ngOnInit():void{
+
+  }
+
+  signUp(data:SignUp):void{
+    this.seller.userSignUP(data).subscribe((result)=>{
+      console.warn(result);
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
+    });
   }
 }
