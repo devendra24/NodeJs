888486c24238e7640c268b4cba2271eb92e0a3f3
Devendra <devendra_dewangan@outlook.com>
2024-01-13 15:10:52 +0530

    home seller 404 routing

--- Angular/ecom-proj/src/app/app.module.ts
+++ Angular/ecom-proj/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { Page404Component } from './page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SellerAuthComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
